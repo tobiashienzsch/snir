c15c54c73021f16343bc6cec7a25a46b7fd5ac8f
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-08-07 21:48:12 +0200

    Use C++23 is_scoped_enum_v

--- src/snir/core/Concepts.hpp
+++ src/snir/core/Concepts.hpp
@@ -5,17 +5,7 @@
 
 namespace snir {
 
-namespace detail {
-
-template<typename E>
-inline constexpr bool IsScopedEnum = requires {
-    requires std::is_enum_v<E>;
-    requires not std::is_convertible_v<E, std::underlying_type_t<E>>;
-};
-
-}  // namespace detail
-
 template<typename Enum>
-concept ScopedEnum = detail::IsScopedEnum<Enum>;
+concept ScopedEnum = std::is_scoped_enum_v<Enum>;
 
 }  // namespace snir
