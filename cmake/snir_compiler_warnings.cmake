5d3b59a40c8757e545850b1e6094d5319144ff6d
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-08-10 21:40:13 +0200

    Add "-Wsign-conversion" for Clang

--- cmake/snir_compiler_warnings.cmake
+++ cmake/snir_compiler_warnings.cmake
@@ -2,34 +2,37 @@ add_library(snir_compiler_warnings INTERFACE)
 add_library(snir::compiler_warnings ALIAS snir_compiler_warnings)
 
 if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC"))
-    target_compile_options(snir_compiler_warnings INTERFACE /W4)
+    target_compile_options(snir_compiler_warnings INTERFACE "/W4")
 else ()
     target_compile_options(snir_compiler_warnings
         INTERFACE
-            -Wall
-            -Wextra
-            -Wpedantic
+            "-Wall"
+            "-Wextra"
+            "-Wpedantic"
 
-            -Wcast-align
-            -Wconversion
-            -Woverloaded-virtual
-            -Wreorder
-            -Wshadow
-            -Wsign-compare
-            # -Wsign-conversion
-            -Wstrict-aliasing
-            -Wswitch-enum
-            -Wuninitialized
-            -Wunreachable-code
-            -Wunused-parameter
-            -Wzero-as-null-pointer-constant
-            -Wredundant-decls
+            "-Wcast-align"
+            "-Wconversion"
+            "-Woverloaded-virtual"
+            "-Wredundant-decls"
+            "-Wreorder"
+            "-Wshadow"
+            "-Wsign-compare"
+            "-Wstrict-aliasing"
+            "-Wswitch-enum"
+            "-Wuninitialized"
+            "-Wunreachable-code"
+            "-Wunused-parameter"
+            "-Wzero-as-null-pointer-constant"
 
-            -Wno-switch-enum
+            "-Wno-switch-enum"
     )
 
-    if(WIN32 AND (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"))
-        # Warns for [[no_unique_address]] being ignored for ABI reasons
-        target_compile_options(snir_compiler_warnings INTERFACE "-Wno-unknown-attributes")
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        target_compile_options(snir_compiler_warnings INTERFACE "-Wsign-conversion")
+
+        if(WIN32)
+            # Warns for [[no_unique_address]] being ignored for ABI reasons
+            target_compile_options(snir_compiler_warnings INTERFACE "-Wno-unknown-attributes")
+        endif()
     endif()
 endif ()
